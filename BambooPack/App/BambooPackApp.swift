ba6d4d665fa6ba7a1af23bad41f58ac8c2d27860
Jiyu Mochi He <jiyuh95@gmail.com>
2026-02-20 19:41:57 -0800

    chore: lan config

--- BambooPack/App/BambooPackApp.swift
+++ BambooPack/App/BambooPackApp.swift
@@ -9,5 +9,26 @@ struct BambooPackApp: App {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+        .commands {
+            CommandGroup(replacing: .appInfo) {
+                Button("About BambooPack") {
+                    NSApplication.shared
+                        .orderFrontStandardAboutPanel(
+                            options: [
+                                .applicationName: "BambooPack",
+                                .credits: NSAttributedString(
+                                    string: "Copyright © 2026, RedPanda Mochi\nLicensed under the MIT license. See LICENSE file.",
+                                    attributes: [
+                                        .font: NSFont.systemFont(ofSize: NSFont.smallSystemFontSize),
+                                        .foregroundColor: NSColor.secondaryLabelColor
+                                    ]
+                                ),
+                                .version: "0.9.0-beta",
+                            ]
+                        )
+                }
+                .keyboardShortcut(",")
+            }
+        }
     }
 }
